1d0ecdfaf9ff20cdaa5504f8ee963a9a80043533
Lars Uffmann <lars.uffmann@gmail.com>
2024-12-06 12:33:29 +0100

    Iniial commit

---
+++ src/main/java/org/cachescrubber/demo_maven_mockito/DemoMavenMockitoApplication.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.demo_maven_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cachescrubber/demo_maven_mockito/DemoMavenMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.demo_maven_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMavenMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
